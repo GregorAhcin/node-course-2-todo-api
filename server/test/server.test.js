3f62fc6cf3cb86842a04bda066caa330dafcfc84
Gregor Ahcin <bubi.grega7@gmail.com>
2018-09-23 10:30:23 +0200

    added to
    test

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,14 +1,17 @@
 const expect = require("expect");
 const request = require("supertest");
+const { ObjectID } = require("mongodb");
 
 const { app } = require("./../server");
 const { Todo } = require("./../models/models");
 
 const testTodos = [
   {
+    _id: new ObjectID(),
     text: "first test insert"
   },
   {
+    _id: new ObjectID(),
     text: "second test insert"
   }
 ];
@@ -75,6 +78,32 @@ describe("GET /todos", () => {
       .expect(res => {
         expect(res.body.todos.length).toBe(2);
       })
+      .end(done);
+  });
+});
+
+describe("GET /todo by id", () => {
+  it("should return todo doc", done => {
+    request(app)
+      .get(`/todos/${testTodos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo.text).toBe(testTodos[0].text);
+      })
+      .end(done);
+  });
+
+  it("should return 404 if todo not found", done => {
+    request(app)
+      .get(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it("should return 404 for no object ids", done => {
+    request(app)
+      .get("/todos/777")
+      .expect(404)
       .end(done);
   });
 });
