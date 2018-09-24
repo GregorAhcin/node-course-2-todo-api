39275cabd0a8e781c47466af48fc220594c6d6bb
Gregor Ahcin <bubi.grega7@gmail.com>
2018-09-24 19:27:34 +0200

    added DELETE tests

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -107,3 +107,46 @@ describe("GET /todo by id", () => {
       .end(done);
   });
 });
+
+describe("DELETE /todos/:id", () => {
+  it("should delete todo", done => {
+    let hexId = testTodos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId)
+          .then(todo => {
+            expect(todo).toNotExist();
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
+
+  it("should return 404 if todo not found", done => {
+    let newId = new ObjectID();
+
+    request(app)
+      .delete(`/todos/${newId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it("should return 404 if object id is invalid", done => {
+    let id = "not_A_Valid_Id";
+
+    request(app)
+      .delete(`/todos/${id}`)
+      .expect(404)
+      .end(done);
+  });
+});
